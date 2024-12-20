6ad800efc54ef4b24b268f53f928b5ac9661de1d
Namithayelugam <138512936+Namithayelugam@users.noreply.github.com>
2024-12-20 15:01:14 +0530

    Module 6,7,8

---
+++ Navbar.js
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+export const Navbar = () => {
+    return (
+        <div>
+        NAVBAR
+        <Link to="/">Home</Link>
+        <Link to="/menu">Home</Link>
+        <Link to="/contact">Home</Link>
+      </div>
+
+    );
+}
